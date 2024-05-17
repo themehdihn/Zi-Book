975fabd9adf4761c9a4fbe577ea628f710f28657
themehdihn <themehdihn@gmail.com>
2024-05-18 00:20:41 +0330

    Sort profile screen

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,18 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View, StyleSheet, Button, ScrollView, Dimensions, Text, StatusBar, TextInput, TouchableWithoutFeedback, Image, ScrollViewBase, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { useNavigation, useIsFocused } from '@react-navigation/native';
-import ZiText from '../components/modules/ZiBookText'
 import { ScaledSize, ScaledWidth, ScaledHeight } from "../utils/responsive"
-import { Icon } from '../components/modules/AppIcon';
-import { useDispatch, useSelector } from "react-redux";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ProfileBox from '../components/templates/Profile/ProfileBox';
 import Theme from '../components/templates/Profile/Theme/Theme';
 import Setting from '../components/templates/Profile/Setting';
 import MoreItems from '../components/templates/Profile/MoreItems';
-import Header from '../components/modules/Header';
 import CopyRight from '../components/templates/Profile/CopyRight';
-import { Profile } from '../redux/store/users';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function ProfileScreen({ navigation }) {
